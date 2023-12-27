8f6a3e530b6271fd9d17f94ae13bd862958a8556
Aljoscha Nyang <a.nyang@gmx.de>
2023-12-27 21:46:30 +0100

    Add gameplay logic

--- main.js
+++ main.js
@@ -1,117 +1,137 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require("prompt-sync")({ sigint: true });
 
-const hat = '^';
-const hole = 'O';
-const fieldCharacter = '░';
-const pathCharacter = '*';
-const directionRegex = /[DLRU]/gi;
+const hat = "^";
+const hole = "O";
+const fieldCharacter = "░";
+const pathCharacter = "*";
 
 const testArray = [
-    [pathCharacter, fieldCharacter, hole],
-    [fieldCharacter, hole, fieldCharacter],
-    [fieldCharacter, hat, fieldCharacter]
-]
+  [pathCharacter, fieldCharacter, hole],
+  [fieldCharacter, hole, fieldCharacter],
+  [fieldCharacter, hat, fieldCharacter],
+];
 
 class Field {
-    constructor(fieldArray) {
-        this.fieldArray = fieldArray;
-        this.maxPositionX = fieldArray[0].length -1;
-        this.maxPositionY = -(fieldArray.length -1);
-    }
+  constructor(array) {
+    this.array = array;
+    this.maxPositionX = array[0].length - 1;
+    this.maxPositionY = array.length - 1;
+  }
 
-    print() {
-        this.fieldArray.forEach(line => {
-            let fieldLine = "";
-            line.forEach(item => fieldLine += item);
+  print() {
+    this.array.forEach((line) => {
+      let fieldLine = "";
+      line.forEach((item) => (fieldLine += item));
 
-            console.log(fieldLine);
-        });
-    }
+      console.log(fieldLine);
+    });
+  }
 }
 
 const getField = (arr) => {
-    const myField = new Field(arr)
-    myField.print();
+  const myField = new Field(arr);
+  myField.print();
 
-    return myField;
-}
+  return myField;
+};
 
-const isValidInput = input => {
-    if(input.length === 1) {
-        return directionRegex.test(input)
-    }
-
-    return false;
-}
+const isValidInput = (input) => {
+  const directionRegex = /[DLRU]/gi;
+  return input.length === 1 && directionRegex.test(input);
+};
 
 const getUserDirection = () => {
-    let validUserInput = false;
+  let validUserInput = false;
 
-    while(!validUserInput) {
-        let direction = prompt('Choose a direction (u=up, r=right, d=down, l=left)');
+  while (!validUserInput) {
+    let direction = prompt(
+      "Choose a direction (u=up, r=right, d=down, l=left)"
+    );
 
-        if(isValidInput(direction)) {
-            return direction.toLowerCase();
-        }
+    if (isValidInput(direction)) {
+      return direction.toLowerCase();
     }
-}
+  }
+};
 
-/*
+const getInitialPlayerPosition = (arr) => {
+  const initialPosition = {};
 
-- get current position (x,y)
-
-2. move player
-- left = x -1
-- right = x +1
-- up = y +1
-- down = y -1
-- arr(x,y) = pathCharacter
-
-*/ 
-
-const getInitialPlayerPosition = arr => {
-    const initialPosition = {};
-
-    for(let i = 0; i < arr.length; i++) {
-        for(let j = 0; j < arr[i].length; j++) {
-            if(arr[i][j] === pathCharacter) {
-                initialPosition.x = j;
-                initialPosition.y = i;
-            }
-        }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] === pathCharacter) {
+        initialPosition.x = j;
+        initialPosition.y = i;
+      }
     }
+  }
 
-    return initialPosition;
-}
+  return initialPosition;
+};
 
-const changePosition = (position, direction) => {
-    switch(direction) {
-        case "u":
-            position.y += 1;
-            break;
-        case "d":
-            position.y -= 1;
-            break;
-        case "r":
-            position.x += 1;
-            break;
-        case "l":
-            position.x -= 1;
-            break;
-        default:
-            break;
-    }
-
-    return position;
-}
-
-// const checkSurroundings = () => {
-
+// const checkSurroundings = (arr, position) => {
+//     if(arr[position.y][position.x] ===  0)
 // }
 
+const changePosition = (position, direction) => {
+  switch (direction) {
+    case "u":
+      position.y -= 1;
+      break;
+    case "d":
+      position.y += 1;
+      break;
+    case "r":
+      position.x += 1;
+      break;
+    case "l":
+      position.x -= 1;
+      break;
+    default:
+      break;
+  }
 
-const playGame = (fieldArr) => {
-    let currentField = getField(fieldArr);
-    let playerPosition = getInitialPlayerPosition(fieldArr);
-    
-}
+  return position;
+};
+
+const playGame = (arr) => {
+  let currentField = getField(arr);
+  let currentArr = arr;
+  let playerPosition = getInitialPlayerPosition(arr);
+  let gameover = false;
+  let message = "";
+
+  while (!gameover) {
+    playerPosition = changePosition(playerPosition, getUserDirection());
+
+    if (
+      playerPosition.x < 0 ||
+      playerPosition.x > currentField.maxPositionX ||
+      playerPosition.y < 0 ||
+      playerPosition.y > currentField.maxPositionY
+    ) {
+      message = "You felt from the edge of the field! Maybe next time!";
+      gameover = true;
+      break;
+    }
+
+    switch (currentArr[playerPosition.y][playerPosition.x]) {
+      case hole:
+        message = "You felt into the hole! Maybe next time!";
+        gameover = true;
+        break;
+      case hat:
+        message = "You found the hat!";
+        gameover = true;
+        break;
+      default:
+        currentArr[playerPosition.y][playerPosition.x] = pathCharacter;
+        currentField = getField(currentArr);
+    }
+  }
+
+  console.log(message);
+  console.log("The game has finished");
+};
+
+playGame(testArray);
