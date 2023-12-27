c0923d25e6f919244b2fd354a49a7ee8a0d91f16
Aljoscha Nyang <a.nyang@gmx.de>
2023-12-27 10:33:16 +0100

    Create Field class

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
 
+class Field {
+    constructor(field) {
+        this.field = field;
+    }
+}
+
+const myField = new Field([
+    [pathCharacter, fieldCharacter, hole],
+    [fieldCharacter, hole, fieldCharacter],
+    [fieldCharacter, hat, fieldCharacter]
+])
